6fb49382e0166ee29e8e71a1c8c72f10d3a2c4e0
Matt Campbell <cmampbell@gmail.com>
2023-03-04 20:38:55 -0500

    finished step 2

--- step2.js
+++ step2.js
@@ -1,15 +1,36 @@
 const fs = require('fs');
-const axios = require('axios')
+const axios = require('axios');
 
-function cat(path) {
+function fileCat(path) {
     fs.readFile(path, 'utf8', (err, data) => {
         if (err){
-            console.log(`Error reading ${path}:` , err)
+            console.log(`Error reading ${path}:` , err);
         }
-        console.log(data)
+        console.log(data);
     })
 }
 
+async function webCat(url) {
+    try {
+        const { data } = await axios.get(url);
+        console.log(data);
+    } catch(err){
+        console.log(`Error fetching ${url}`, err.code)
+    }
+}
+
+function isValidURL(input) {
+    try {
+        new URL(input);
+        return true;
+    } catch(err) { return false; }
+}
+
 for(let i = 2; i < process.argv.length; i++) {
-    cat(process.argv[i])
+
+    if(isValidURL(process.argv[i])){
+        webCat(process.argv[i]);
+    } else {
+        fileCat(process.argv[i]);
+    }
 }
